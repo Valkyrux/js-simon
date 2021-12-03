34ea3ef75241b9631dfdc77a202c98a65087e254
Valkyrux <philadephos.granata@gmail.com>
2021-12-03 17:55:47 +0100

    Bonus

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
 // assegno le costanti del mio script
-const NumberOfNumber = 5;
-const delay = 30;
+let difficuly = prompt("inserisci la difficoltà (facile, medio, difficile)");
+const delay = 3;
+const maxNumber = 100;
 const numbersOnDom = document.getElementById("random-numbers");
 numbersOnDom.style = "text-align: center; color: red;"
-// funzione che genera n prompt
-function getPrompt (numOfPrompt) {
-    let choosedNumber = [];
-    for (let i = 0; i < numOfPrompt; i++) {
-        choosedNumber[i] = parseInt(prompt("Inserisci un numero"));
+while (difficuly != "facile" && difficuly != "medio" && difficuly != "difficile") {
+    difficuly = prompt("inserisci la difficoltà (facile, medio, difficile)");
+}
+let NumberOfNumber;
+
+switch (difficuly) {
+    case "facile":
+        NumberOfNumber = 5;
+        break 
+    case "medio":
+        NumberOfNumber = 10;
+        break 
+    case "difficile":
+        NumberOfNumber = 20;
+        break 
+} 
+
+// funzione che genera n prompt e controlla che siano diversi e controllo che siano numeri maggiori di 0 e minori di maxNumber
+function getPrompt (numOfPrompt, maxPromptValue) {
+    let choosedNumbers = [];
+    while (choosedNumbers.length < numOfPrompt) {
+        let aPromptNumber = parseInt(prompt("Inserisci un numero"));
+        if (!choosedNumbers.includes(aPromptNumber) && aPromptNumber > 0 && aPromptNumber <= maxPromptValue) {
+            choosedNumbers.push(aPromptNumber);
+        } else {
+            let aNewPromptNumber = aPromptNumber;
+            while(choosedNumbers.includes(aNewPromptNumber) || aNewPromptNumber <= 0 || aNewPromptNumber > maxPromptValue) {
+                aNewPromptNumber = parseInt(prompt("numero non valido riprovare!")); 
+            }
+            choosedNumbers.push(aNewPromptNumber);
+        }
     } 
-    return choosedNumber;
+    return choosedNumbers;
 }
 // funzione che genera un array di lungezza arrayLength che appende i suoi valori all'interno del DOM
 function getRandArray(arrayLength, minValue, maxValue) {
@@ -22,26 +49,30 @@ function getRandArray(arrayLength, minValue, maxValue) {
     }
     return randArray;
 }
-
+// funzione che controlla gli array e assegna il punteggio, tiene anche conto dei doppioni (lo avevo fatto prima di vedere i bonus OMG)
 function getResult() {
-    const inputValues = getPrompt(NumberOfNumber);
+    const inputValues = getPrompt(NumberOfNumber, maxNumber);
     let score = 0;
     for (let i = 0; i < NumberOfNumber; i++) {
-        if(randNumbers[i] == inputValues[i]) {
-            console.log(inputValues[i], "   ",randNumbers[i], ': corretto');
+        let uniquenessCheck = true;
+        for (let k = 0; k < i; k++) {
+            if(inputValues[k] == inputValues[i]){
+                uniquenessCheck = false;
+            }
+        }
+        if(uniquenessCheck && randNumbers.includes(inputValues[i])) {
+            console.log(randNumbers[i], "     ", inputValues[i],': corretto');
             score++;
         } else {
-            console.log(inputValues[i], "   ",randNumbers[i], ': sbagliato');
+            console.log(randNumbers[i], "     ", inputValues[i],': sbagliato');
         }
     }
     return console.log("punteggio:", score);
 }
-
-console.log("Tu    Simon");
+// scipt che chiama le funzioni
+console.log("Tu      Simon");
 const randNumbers = getRandArray(NumberOfNumber, 1, 100);
 numbersOnDom.append(randNumbers);
-setTimeout(() => {numbersOnDom.innerHTML = ""}, 3000);
+setTimeout(() => {numbersOnDom.innerHTML = ""}, 2000);
 // ritardo per l'input
 const timer = setTimeout(getResult, delay*1000);
-
-
