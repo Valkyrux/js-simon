d50c79f25dbf335af85f43dcd16ce998c8342303
Valkyrux <philadephos.granata@gmail.com>
2021-12-03 16:34:57 +0100

    commenti

--- js/script.js
+++ js/script.js
@@ -1,41 +1,47 @@
+// assegno le costanti del mio script
 const NumberOfNumber = 5;
-const randNumbers = [];
 const delay = 30;
 const numbersOnDom = document.getElementById("random-numbers");
 numbersOnDom.style = "text-align: center; color: red;"
-
-function getPrompt () {
+// funzione che genera n prompt
+function getPrompt (numOfPrompt) {
     let choosedNumber = [];
-    for (let i = 0; i < NumberOfNumber; i++) {
+    for (let i = 0; i < numOfPrompt; i++) {
         choosedNumber[i] = parseInt(prompt("Inserisci un numero"));
     } 
     return choosedNumber;
 }
+// funzione che genera un array di lungezza arrayLength che appende i suoi valori all'interno del DOM
+function getRandArray(arrayLength, minValue, maxValue) {
+    const randArray = [];
+    while (randArray.length < arrayLength) {
+        let rand = Math.floor(Math.random()*(maxValue - minValue + 1) + minValue);
+        if (!randArray.includes(rand)) {
+            randArray.push(rand);
+        }
+    }
+    return randArray;
+}
 
-function getResult () {
-    const inputValues = getPrompt();
+function getResult() {
+    const inputValues = getPrompt(NumberOfNumber);
     let score = 0;
     for (let i = 0; i < NumberOfNumber; i++) {
-        if(inputValues[i] == randNumbers[i]) {
-            console.log(inputValues[i], "=", randNumbers[i], ": corretto");
+        if(randNumbers[i] == inputValues[i]) {
+            console.log(inputValues[i], "   ",randNumbers[i], ': corretto');
             score++;
         } else {
-            console.log(inputValues[i], "=", randNumbers[i], ": sbagliato");
+            console.log(inputValues[i], "   ",randNumbers[i], ': sbagliato');
         }
     }
     return console.log("punteggio:", score);
 }
 
-
-
-while (randNumbers.length < NumberOfNumber) {
-    let rand = Math.floor(Math.random()*100) + 1;
-    if (!randNumbers.includes(rand)) {
-        randNumbers.push(rand);
-        numbersOnDom.append(rand + " ");
-    }
-}
-setTimeout(() => {numbersOnDom.style = "display: none"}, 2000);
-
+console.log("Tu    Simon");
+const randNumbers = getRandArray(NumberOfNumber, 1, 100);
+numbersOnDom.append(randNumbers);
+setTimeout(() => {numbersOnDom.innerHTML = ""}, 3000);
+// ritardo per l'input
 const timer = setTimeout(getResult, delay*1000);
+
 
