9fc2ca40ad71ece41d61e3e12f3b5fcabadb8ad4
Valkyrux <philadephos.granata@gmail.com>
2021-12-03 15:48:42 +0100

    esercizio-finito

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 const NumberOfNumber = 5;
 const randNumbers = [];
-const numbersOnDom = document.getElementById("random-numbers")
+const delay = 30;
+const numbersOnDom = document.getElementById("random-numbers");
 numbersOnDom.style = "text-align: center; color: red;"
+
+function getPrompt () {
+    let choosedNumber = [];
+    for (let i = 0; i < NumberOfNumber; i++) {
+        choosedNumber[i] = parseInt(prompt("Inserisci un numero"));
+    } 
+    return choosedNumber;
+}
+
+function getResult () {
+    const inputValues = getPrompt();
+    let score = 0;
+    for (let i = 0; i < NumberOfNumber; i++) {
+        if(inputValues[i] == randNumbers[i]) {
+            console.log(inputValues[i], "=", randNumbers[i], ": corretto");
+            score++;
+        } else {
+            console.log(inputValues[i], "=", randNumbers[i], ": sbagliato");
+        }
+    }
+    return console.log("punteggio:", score);
+}
+
+
 
 while (randNumbers.length < NumberOfNumber) {
     let rand = Math.floor(Math.random()*100) + 1;
@@ -10,5 +35,7 @@ while (randNumbers.length < NumberOfNumber) {
         numbersOnDom.append(rand + " ");
     }
 }
+setTimeout(() => {numbersOnDom.style = "display: none"}, 2000);
 
-setTimeout(() => {numbersOnDom.style = "display: none"}, 3000);
+const timer = setTimeout(getResult, delay*1000);
+
